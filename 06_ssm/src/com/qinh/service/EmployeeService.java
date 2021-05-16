5c9d236a7473cfe2e7ed4b46fbcacd1f5f34fcbf
Qh92 <771600967@qq.com>
2021-05-16 23:44:22 +0800

    fix:修复部分bug

--- 06_ssm/src/com/qinh/service/EmployeeService.java
+++ 06_ssm/src/com/qinh/service/EmployeeService.java
@@ -2,6 +2,7 @@ package com.qinh.service;
 
 import com.qinh.dao.EmployeeMapper;
 import com.qinh.entity.Employee;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,12 @@ public class EmployeeService {
     @Autowired
     EmployeeMapper employeeMapper;
 
+    @Autowired
+    private SqlSession sqlSession;
+
     public List<Employee> listEmployees(){
+        //在这里可以进行批量操作
+        //EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
         List<Employee> employees = employeeMapper.listEmployees();
         return employees;
     }
